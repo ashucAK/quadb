e5234e109a0bda3a3c59109ac4c906e33f9d5277
ashucAK <singhashishk12@gmail.com>
2024-04-15 14:49:08 +0530

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'; // Import React
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import TodoApp from './components/TodoApp';
 
-const App = () => {
+const App: React.FC = () => { // Ensure to use React.FC for App component
   return (
     <Provider store={store}>
       <div className="flex items-center justify-center min-h-screen bg-gray-200">
